3a9cdd711bb083202744e7ab165404b591225445
MystiaFin <cytrsx01@gmail.com>
2024-08-03 18:51:39 +0700

    fixed header post text input

--- src/components/HeaderPost.tsx
+++ src/components/HeaderPost.tsx
@@ -1,14 +1,13 @@
 import avatarImage from '../assets/avatar.png';
 import { useState } from 'react';
+import NewPostModal from './NewPostModal';
 
 function HeaderPost() {
     const [text, setText] = useState('');
-
-    const handleInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-        setText(event.target.value);
-        event.target.style.height = 'auto';
-        event.target.style.height = `${event.target.scrollHeight}px`;
-    };
+    const [modal, setModal] = useState(false)
+    const toggleModal = ()  => {
+        setModal(!modal)
+    }
 
     return (
         <div className='bg-gray-900 p-4 rounded-xl mb-6 '>
@@ -16,13 +15,9 @@ function HeaderPost() {
                 <img src={avatarImage} className='w-16 rounded-full' alt="Avatar"/>
                 
                 <div className='flex-1 flex flex-col'>
-                    <textarea
-                        className='w-auto bg-gray-800 text-white lg:text-base md:text-base sm:text-xs placeholder-gray-400 rounded-lg px-6 py-3 focus:outline-none resize-none overflow-hidden'
-                        placeholder="What's on your mind?"
-                        value={text}
-                        onChange={handleInputChange}
-                        rows={1} // Start with a single line
-                    />
+                <button onClick={toggleModal} className="bg-gray-800 rounded-full lg:text-base md:text-base sm:text-xs pl-4 text-gray-500 lg:py-2 md:py-0 sm:py-0 flex items-center">
+                    <span className="hidden lg:inline">What's on your mind?</span>
+                </button>
                     <div className='flex gap-5 mt-3 text-gray-400 text-sm'>
                         <button className='flex items-center bg-black p-2 px-4 rounded-full'>
                             <i className="ri-image-line mr-2"></i>
@@ -35,6 +30,7 @@ function HeaderPost() {
                     </div>
                 </div>
             </div>
+            <NewPostModal isOpen={modal} onClose={toggleModal} />
         </div>
     );
 }
