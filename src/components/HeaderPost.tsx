15c4a0ef4b43abf85146b0af9a19c22de88026ca
MystiaFin <cytrsx01@gmail.com>
2024-08-03 19:04:51 +0700

    fixed headerpost button disappear in small screen

--- src/components/HeaderPost.tsx
+++ src/components/HeaderPost.tsx
@@ -12,10 +12,9 @@ function HeaderPost() {
         <div className='bg-gray-900 p-4 rounded-xl mb-6 '>
             <div className='flex items-center gap-3'>
                 <img src={avatarImage} className='w-16 rounded-full' alt="Avatar"/>
-                
                 <div className='flex-1 flex flex-col'>
-                <button onClick={toggleModal} className="bg-gray-800 rounded-full lg:text-base md:text-base sm:text-xs pl-4 text-gray-500 lg:py-2 md:py-0 sm:py-0 flex items-center">
-                    <span className="hidden lg:inline">What's on your mind?</span>
+                <button onClick={toggleModal} className="bg-gray-800 rounded-full pl-4 text-gray-500 py-2 flex items-center">
+                    <span className="">What's on your mind?</span>
                 </button>
                     <div className='flex gap-5 mt-3 text-gray-400 text-sm'>
                         <button className='flex items-center bg-black p-2 px-4 rounded-full'>
